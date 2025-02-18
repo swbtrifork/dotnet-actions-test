5f8eeb613e03d242ee10bd213cfe4f62c423df72
Simon Wessmann Brok <swb@trifork.com>
2025-02-18 14:25:09 +0100

    Change stuff

---
+++ test.py
@@ -0,0 +1,26 @@
+# Creat object '{"energi-danmark": "docker-image-1", "next": "docker-image-2"}'
+
+import json
+
+data = {
+    "energi-danmark": "docker-image-1",
+    "next": "docker-image-2"
+}
+
+# split files string into list. Divide by spaces
+files = ["src/base/energi-danmark/hej.txt", "src/base/next/hej.txt", "src/base/next/hej2.txt", "./github/workflows/main.yml"]
+
+images = []
+
+
+for file in files:
+    # Remove hej.txt from file
+    path = file.split("/")[0:3]
+    path = "/".join(path) + "/Dockerfile"
+    if(path not in images and path.startswith("src")):
+        images.append(path)
+
+print(images)
+    
+
+
