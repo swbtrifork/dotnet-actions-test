484c2ae8d603195127b915f0ea5580b3b7b9ce5c
Simon Wessmann Brok <swb@trifork.com>
2025-02-18 13:11:19 +0100

    Try

--- src/base/energi-danmark/Program.cs
+++ src/base/energi-danmark/Program.cs
@@ -23,6 +23,5 @@ var content = await response.Content.ReadAsStringAsync();
 
 producer.Produce(topic, new Message<string, string> { Key = null, Value = content });
 
-
 producer.Flush(TimeSpan.FromSeconds(10));
 // Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
